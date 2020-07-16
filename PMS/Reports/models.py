1eb6428dbe0eb5f7dda531d62b02aec9fdc89716
LaraibHere <laraibshazil@gmail.com>
2020-07-16 22:25:59 +0500

    initial commit

--- PMS/Reports/models.py
+++ PMS/Reports/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.db.models import IntegerField
 
-# Create your models here.
+
 class Person(models.Model):
     name = models.CharField(max_length=100)
     age = models.PositiveIntegerField()
@@ -15,12 +16,13 @@ class Patient(Person):
     id=models.IntegerField(primary_key=True)
     disease = models.CharField(max_length=500)
     previousHistory = models.TextField()
-    doctor = models.CharField(max_length=50)
-    gender = models.CharField(max_length=8)
+    doctor = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100)
+
 
 class Doctor(Person):
     id=models.IntegerField(primary_key=True)
-    specialization = models.TextField(max_length=200)
+    specialization = models.TextField(max_length=500)
     on_duty = models.BooleanField(default=False)
 
 class Admin(Person):
